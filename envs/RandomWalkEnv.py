c0e9ff8cbef8a34433342412298fa2517c1fb972
Adrian Skrobacz <adik993@gmail.com>
2017-09-27 23:38:05 +0200

    Gradient Monte Carlo: Algorithm seems to work, but not perfectly

--- envs/RandomWalkEnv.py
+++ envs/RandomWalkEnv.py
@@ -2,19 +2,22 @@ from gym import Env
 from gym.spaces import Discrete
 import numpy as np
 
+from envs.WindyGridWorldEnv import minmax
+
 
 class RandomWalk(Env):
     metadata = {'render.modes': ['human']}
     ACTION_LEFT = 0
     ACTION_RIGHT = 1
 
-    def __init__(self, n_states=7, left_reward=0, right_reward=1, start_position=3):
+    def __init__(self, n_states=7, left_reward=0, right_reward=1, start_position=3, max_step=1):
         self.states = np.zeros((n_states,))
         self.states[0] = left_reward
         self.states[-1] = right_reward
         self.start_position = len(self.states) // 2 if start_position is None else start_position
         self.action_space = Discrete(2)
         self.observation_space = Discrete(len(self.states))
+        self.steps = np.arange(1, max_step + 1)
         self._reset()
 
     def _render(self, mode='human', close=False):
@@ -30,15 +33,20 @@ class RandomWalk(Env):
         return self.position
 
     def _step(self, action):
+        step = self._get_step_size()
         if action == RandomWalk.ACTION_LEFT:
-            self.position -= 1
+            self.position -= step
         else:
-            self.position += 1
+            self.position += step
+        self.position = minmax(self.position, 0, len(self.states) - 1)
 
         done = self.position == 0 or self.position == len(self.states) - 1
         reward = self.states[self.position]
 
         return self._observations(), reward, done, self.states
+
+    def _get_step_size(self):
+        return np.random.choice(self.steps) if len(self.steps) > 1 else 1
 
 
 if __name__ == '__main__':
---
+++ gradient_methods_random_walk.py
@@ -0,0 +1,103 @@
+from gym import Env
+
+from envs.CliffWalkingEnv import minmax
+from envs.RandomWalkEnv import RandomWalk
+import numpy as np
+import plotly.offline as py
+import plotly.graph_objs as go
+
+N_AGGREGATE = 100
+N_STATES = 1000
+MAX_STEP = 100
+
+
+def find_true_values():
+    new = np.arange(-N_STATES - 1, N_STATES + 3, 2) / (N_STATES + 1)
+    while True:
+        old = new.copy()
+        for state in range(1, N_STATES + 1):
+            new[state] = 0
+            for action in [-1, 1]:
+                for step in range(1, MAX_STEP + 1):
+                    step *= action
+                    next_state = minmax(state + step, 0, N_STATES + 1)
+                    prob = 1 / (MAX_STEP * 2)
+                    new[state] += prob * (0 + new[next_state])
+        error = np.abs(np.sum(old - new))
+        print('Ture values error:', error)
+        if error < 1e-2:
+            break
+    return new
+
+
+TRUE_VALUES = find_true_values()
+
+
+class State:
+    def __init__(self, state, reward):
+        self.state = state
+        self.reward = reward
+
+
+class EpisodeAlgorithm:
+    def action(self, state):
+        raise NotImplementedError()
+
+    def on_new_episode(self, history):
+        raise NotImplementedError()
+
+
+class ValueFunction:
+    def __init__(self, shape, aggregation=N_AGGREGATE):
+        self.value = np.zeros([s // aggregation for s in shape])
+        self.aggregation = aggregation
+
+    def _idx(self, item):
+        return (item - 1) // self.aggregation
+
+    def __getitem__(self, item):
+        return self.value[self._idx(item)]
+
+    def __setitem__(self, key, value):
+        self.value[self._idx(key)] = value
+
+
+class GradientMonteCarlo(EpisodeAlgorithm):
+    def __init__(self, env: Env, alpha=2e-5):
+        self.alpha = alpha
+        self.actions = np.arange(env.action_space.n)
+        self.state_value = ValueFunction((N_STATES,), aggregation=100)
+
+    def action(self, state):
+        return np.random.choice(self.actions)
+
+    def on_new_episode(self, history):
+        # Since intermediate rewards are 0 G_t is equal to reward of terminal state
+        reward = history[-1].reward
+        for state in history[:-1]:
+            self.state_value[state.state] += self.alpha * (reward - self.state_value[state.state])
+
+
+def generate_episode(env: Env, algorithm: EpisodeAlgorithm):
+    history = []
+    done = False
+    obs = env.reset()
+    while not done:
+        prev_obs = obs
+        action = algorithm.action(prev_obs)
+        obs, reward, done, aux = env.step(action)
+        history.append(State(prev_obs, reward))
+    algorithm.on_new_episode(history)
+    return history
+
+
+if __name__ == '__main__':
+    env = RandomWalk(n_states=N_STATES + 2, left_reward=-1, right_reward=1, start_position=None, max_step=MAX_STEP)
+    algorithm = GradientMonteCarlo(env)
+    for i in range(int(1e5)):
+        print('Episode:', i)
+        generate_episode(env, algorithm)
+
+    data = [go.Scatter(y=algorithm.state_value.value.repeat(N_AGGREGATE), name='Gradient Monte Carlo'),
+            go.Scatter(y=TRUE_VALUES, name='True Values')]
+    py.plot(data)
